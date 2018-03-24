697095b36774e356961f6c1295e52341b6f3e503
Artashes Amatuni <artashes.amatuni@gmail.com>
2018-03-25 00:14:20 +0400

    ok

--- localstorage/modules/menu.php
+++ localstorage/modules/menu.php
@@ -4,50 +4,45 @@ include 'settings.php';
 
 function gen_user($user)
 {
-
-echo "<div class=\"w3-left\">
-<div class=\"w3-bar-item\">\n";
-switch ($user)
-{
-    case 'admin':
-        echo "<img src=\"/localstorage/images/img_avatar1.png\" class=\"w3-circle w3-border\" alt=\"Admin\" width=\"24\" height=\"24\"/>";
-        break;
-    case 'user':
-        echo "<img src=\"/localstorage/images/img_avatar2.png\" class=\"w3-circle w3-border\" alt=\"Admin\" width=\"24\" height=\"24\"/>";
-        break;
-    case 'guest':
-        echo "<img src=\"/localstorage/images/img_avatar3.png\" class=\"w3-circle w3-border\" alt=\"Admin\" width=\"24\" height=\"24\"/>";
-        break;
-}
-echo "</div>\n";
-echo "<a href=\"#\" class=\"w3-bar-item\"><i class=\"fa fa-sign-out\"></i></a>";
-
-
+  echo "<a href=\"#\" class=\"w3-bar-item w3-button\">";
+  switch ($user)
+  {
+      case 'admin':
+          echo "<img src=\"/localstorage/images/img_avatar1.png\" class=\"w3-circle w3-border\" alt=\"Admin\" width=\"18\" height=\"18\"/>";
+          break;
+      case 'user':
+          echo "<img src=\"/localstorage/images/img_avatar2.png\" class=\"w3-circle w3-border\" alt=\"Admin\" width=\"18\" height=\"18\"/>";
+          break;
+      case 'guest':
+          echo "<img src=\"/localstorage/images/img_avatar3.png\" class=\"w3-circle w3-border\" alt=\"Admin\" width=\"18\" height=\"18\"/>";
+          break;
+  }
+  echo "</a>";
 }
 
 
 function gen_dropdown($cur)
 {
     if ($cur!='Analog Inputs') {
-        echo "<a href=\"/localstorage/ai.php\" class=\"w3-bar-item w3-button\">Analog Inputs</a>\n";
+        echo "<a href=\"/localstorage/ai.php\" class=\"w3-bar-item w3-button w3-padding-large\">Analog Inputs</a>\n";
     }
     if ($cur!='Digital Inputs') {
-        echo "<a href=\"/localstorage/di.php\" class=\"w3-bar-item w3-button\">Digital Inputs</a>\n";
+        echo "<a href=\"/localstorage/di.php\" class=\"w3-bar-item w3-button w3-padding-large\">Digital Inputs</a>\n";
     }
     if ($cur!='Digital Outputs') {
-        echo "<a href=\"/localstorage/do.php\" class=\"w3-bar-item w3-button\">Digital Outputs</a>\n";
+        echo "<a href=\"/localstorage/do.php\" class=\"w3-bar-item w3-button w3-padding-large\">Digital Outputs</a>\n";
     }
     if ($cur!='MQTT Settings') {
-        echo "<a href=\"/localstorage/modules/mqtt/mqtt_main.php\" class=\"w3-bar-item w3-button\">MQTT Settings</a>\n";
+        echo "<a href=\"/localstorage/modules/mqtt/mqtt_main.php\" class=\"w3-bar-item w3-button w3-padding-large\">MQTT Settings</a>\n";
     }
     if ($cur!='Modbus Settings') {
-        echo "<a href=\"/localstorage/mbus.php\" class=\"w3-bar-item w3-button\">Modbus Settings</a>\n";
+        echo "<a href=\"/localstorage/mbus.php\" class=\"w3-bar-item w3-button w3-padding-large\">Modbus Settings</a>\n";
     }
     if ($cur!='Access control') {
-        echo "<a href=\"/localstorage/modules/access/login_main.php\" class=\"w3-bar-item w3-button\">Access control</a>\n";
+        echo "<a href=\"/localstorage/modules/access/login_main.php\" class=\"w3-bar-item w3-button w3-padding-large\">Access control</a>\n";
     }
     if ($cur!='Settings') {
-        echo "<a href=\"/localstorage/config.php\" class=\"w3-bar-item w3-button\">Settings</a>\n";
+        echo "<a href=\"/localstorage/config.php\" class=\"w3-bar-item w3-button w3-padding-large\">Settings</a>\n";
     }
 }
 
@@ -57,55 +52,56 @@ function show_menu($cur)
     global $max_window;
     global $min_window;
     $user = 2;
-    echo "<div class=\"w3-top w3-light-gray w3-bar\" style=\"max-width:".$max_window."px;min-width:".$min_window."px\">
-          <div id=\"small_menu\" class=\"w3-top w3-bar-block w3-white w3-border w3-hide w3-hide-large w3-hide-medium\">
-              <a href=\"javascript:void(0)\" class=\"w3-bar-item w3-button\" onclick=\"smallMenu()\"><i class=\"fa fa-window-close-o\"></i></a>";
-              gen_dropdown($cur);
-        echo "<a onclick=\"document.getElementById('help').style.display='block'\" class=\"w3-bar-item w3-button\">Help</a>
-          </div>
-          <a href=\"javascript:void(0)\" class=\"w3-bar-item w3-button w3-left w3-hide-large w3-hide-medium\" onclick=\"smallMenu()\"><i class=\"fa fa-navicon\"></i></a>
-          <div class=\"w3-right\">\n";
-          if ($cur!='Dashboard') {
-                echo "<a href=\"/index.php\" class=\"w3-bar-item w3-button\">Dashboard</a>\n";
-            }
-    echo "<div class=\"w3-dropdown-click w3-hide-small\">
-                  <button class=\"w3-button\" onclick=\"dropdown()\">Configuration&nbsp;<i class=\"fa fa-caret-down\"></i></button>
-                  <div id=\"config_menu\" class=\"w3-dropdown-content w3-bar-block w3-border\" style=\"z-index:10\">\n";
 
-    gen_dropdown($cur);
-    echo "</div>
-              </div>
-              <a onclick=\"document.getElementById('help').style.display='block'\" class=\"w3-bar-item w3-button w3-hide-small\"><i class=\"fa fa-question-circle-o\"></i></a>
-              <a href=\"http://www.eaglemon.com\" target=\"_blank\" class=\"w3-bar-item w3-right\">
-                  <image alt=\"logo\" src=\"/localstorage/images/logo_s.png\" style=\"height: 24px\" />
-              </a>
-          </div>\n";
-          gen_user('admin');
-          echo "</div>
-  </div>\n";
-    include_js();
+    # Navbar
+    echo "<!-------------------------------------------------------------------------------------------------------------------------------------------------->
+          <div class=\"w3-top w3-light-gray w3-bar\" style=\"max-width:".$max_window."px;min-width:".$min_window."px\">\n";
+      # Big
+      echo "<div class=\"w3-bar w3-card-4 w3-left-align\">\n";
+      echo "<a href=\"http://www.eaglemon.com\" target=\"_blank\" class=\"w3-bar-item w3-button\"><image alt=\"logo\" src=\"/localstorage/images/logo_s.png\" height=\"18\"/></a>\n";
+      gen_user('admin');
+      echo "<a class=\"w3-bar-item w3-button w3-hide-medium w3-hide-large w3-right\" href=\"javascript:void(0);\" onclick=\"small_drop()\"><i class=\"fa fa-bars\"></i></a>\n";
+      echo "<a class=\"w3-bar-item w3-button w3-hide-small w3-right\" href=\"javascript:void(0);\" onclick=\"big_drop()\">Configuration<i class=\"fa fa-bars\"></i></a>\n";
+      if ($cur!='Dashboard') {
+      echo "<a href=\"/index.php\" class=\"w3-bar-item w3-button\">Dashboard</a>\n";
+    }
+
+
+    echo "<a onclick=\"document.getElementById('help').style.display='block'\" class=\"w3-bar-item w3-button w3-right w3-hover-white\"><i class=\"fa fa-question-circle-o\"></i></a>\n";
+      echo "</div>\n";
+
+        # Small
+        echo "<div id=\"small_nav\" class=\"w3-bar-block w3-border w3-light-gray w3-hide w3-hide-large w3-hide-medium\">\n";
+                gen_dropdown($cur);
+        echo "</div>\n";
+        echo "<div id=\"big_nav\" class=\"w3-bar-block w3-border w3-light-gray w3-hide w3-hide-small\">\n";
+                gen_dropdown($cur);
+        echo "</div>\n";
+      echo "</div>\n";
+
+
+
+echo "<script>
+function small_drop() {
+    var x = document.getElementById(\"small_nav\");
+    if (x.className.indexOf(\"w3-show\") == -1) {
+        x.className += \" w3-show\";
+    } else {
+        x.className = x.className.replace(\" w3-show\", \"\");
+    }
 }
 
-function include_js()
-{
-    echo "\n<script type='text/javascript'>\n";
-    echo "function dropdown() {
-      var x = document.getElementById(\"config_menu\");
-      if (x.className.indexOf(\"w3-show\") == -1) {
-          x.className += \" w3-show\";
-      } else {
-          x.className = x.className.replace(\" w3-show\", \"\");
-      }
-  }\n";
-
-    echo "function smallMenu() {
-      var x = document.getElementById(\"small_menu\");
-      if (x.className.indexOf(\"w3-show\") == -1) {
-          x.className += \" w3-show\";
-      } else {
-          x.className = x.className.replace(\" w3-show\", \"\");
-      }
-  }\n";
-    echo "</script>\n";
+function big_drop() {
+    var x = document.getElementById(\"big_nav\");
+    if (x.className.indexOf(\"w3-show\") == -1) {
+        x.className += \" w3-show\";
+    } else {
+        x.className = x.className.replace(\" w3-show\", \"\");
+    }
 }
+</script>
+<!-------------------------------------------------------------------------------------------------------------------------------------------------->\n";
+
+}
+
 ?>
