585d872058d7610613b3282d696c0df7009a862c
Artashes Amatuni <artashes.amatuni@gmail.com>
2018-04-06 18:14:22 +0400

    ok

--- localstorage/do.php
+++ localstorage/do.php
@@ -9,11 +9,9 @@ $cur = 'Digital Outputs';
 show_menu($cur);
 echo "<div class=\"w3-main\" style=\"height: 100%; margin-top:50px;margin-bottom:64px;\">\n";
 $t_names = array("Relay 0", "Relay 1","Relay 2","Relay 3");
-if (isset($_COOKIE['c_tab']))
-{
+if (isset($_COOKIE['c_tab'])) {
     $cur_tab = $_COOKIE['c_tab'];
-}
-else {
+} else {
     $cur_tab = 1;
 }
 draw_tabs($t_names, $cur_tab);
@@ -26,17 +24,19 @@ end_line();
 function read_config($cur_tab)
 {
     echo "<div class=\"w3-container w3-border-right w3-border-left w3-border-bottom w3-light-gray\">\n";
-    $conn    = Connect();
+    $conn = Connect();
     $sql = "SELECT id, name, enabled, polarity, mode FROM rl_configs";
     $result = $conn->query($sql);
     $conn->close();
     if ($result->num_rows > 0) {
         while ($row = $result->fetch_assoc()) {
-            if ($cur_tab==$row["id"]) {
+            if ($cur_tab == $row["id"]) {
                 echo "<div id=\"tab".($row["id"])."\" class=\"w3-container w3-show\">\n";
             } else {
                 echo "<div id=\"tab".($row["id"])."\" class=\"w3-container w3-hide\">\n";
             }
+            echo "<br/>\n";
+            $row["id"];
             echo "<br/>\n";
             echo "<form method=\"post\" action=\"modules/do/do_save.php\">
             <input name=\"id\" type=\"hidden\" value=\"".($row["id"])."\">
@@ -45,54 +45,54 @@ function read_config($cur_tab)
                         <div class=\"w3-col m2 s2\">
                             <label>Enable</label>
                             <br/>";
-                            if ($row["enabled"]==1) {
-                                echo "<input type=\"checkbox\" class=\"w3-check\" name=\"enabled\" value=\"1\" checked=\"checked\" />";
-                            } else {
-                                echo "<input type=\"checkbox\" class=\"w3-check\" name=\"enabled\" value=\"0\" />";
-                            }
-                    echo "</div>
+            if ($row["enabled"] == 1) {
+                echo "<input type=\"checkbox\" class=\"w3-check\" name=\"enabled\" value=\"1\" checked=\"checked\" />";
+            } else {
+                echo "<input type=\"checkbox\" class=\"w3-check\" name=\"enabled\" value=\"0\" />";
+            }
+            echo "</div>
                     <div class=\"w3-col m2 s2\">
                         <label>Polarity</label>
                         <br/>";
-                        if ($row["polarity"]==1) {
-                            echo "<input type=\"checkbox\" class=\"w3-check\" name=\"polarity\" value=\"1\" checked=\"checked\" />";
-                        } else {
-                            echo "<input type=\"checkbox\" class=\"w3-check\" name=\"polarity\" value=\"0\" />";
-                        }
-                echo "</div>\n";
-                    echo "<div class=\"w3-col m5 s5\">
+            if ($row["polarity"] == 1) {
+                echo "<input type=\"checkbox\" class=\"w3-check\" name=\"polarity\" value=\"1\" checked=\"checked\" />";
+            } else {
+                echo "<input type=\"checkbox\" class=\"w3-check\" name=\"polarity\" value=\"0\" />";
+            }
+            echo "</div>\n";
+            echo "<div class=\"w3-col m5 s5\">
                             <label>Name</label>
                             <input name=\"name\" class=\"w3-input w3-border\" type=\"text\" placeholder=\"e.g. Room temperature\" value=\"".$row["name"]."\" />
                           </div>\n";
-                    echo "<div class=\"w3-col m3 s3\">";
-                            require_once 'modules/do/do_modes.php';
-                    echo "</div>
+            echo "<div class=\"w3-col m3 s3\">";
+            do_modes($row["mode"]);
+            echo "</div>
                     </div>
                     <br/>\n";
-                    echo "<div class=\"w3-row-padding\">
+            echo "<div class=\"w3-row-padding\">
                             <div class=\"w3-col m6 s6\">";
-                                    require_once 'modules/do/do_operators.php';
-                    echo "</div>
+            do_operators();
+            echo "</div>
                             <div class=\"w3-col m6 s6\">";
-                            require_once 'modules/do/do_sources.php';
-                            echo "</div>
+            do_sources();
+            echo "</div>
                         </div>
                         <br/>";
 
 
-                    echo "<div class=\"w3-row-padding\">
+            echo "<div class=\"w3-row-padding\">
                             <div class=\"w3-col m12 s12\">
                                     <input type=\"submit\" name=\"insert".$row["id"]."\" class=\"w3-button w3-block w3-green\" value=\"Save\" />
                             </div>
                         </div>";
-                echo "</form>
+            echo "</form>
                       <br/>
                     </div>\n";
         }
     }
     echo "</div>\n";
 }
-
+/*
 if (isset($_POST['insert0'])) {
     save(0);
 }
@@ -106,28 +106,66 @@ if (isset($_POST['insert3'])) {
     save(3);
 }
 
-
-function save($id)
+*/
+function do_modes($mode)
 {
-    if (isset($_POST['enabled'])) {
-        $enabled=1;
-    } else {
-        $enabled=0;
-    }
-    if (isset($_POST['polarity'])) {
-        $polarity=1;
-    } else {
-        $polarity=0;
-    }
-    $conn    = Connect();
-    $name       = $conn->real_escape_string($_POST['name']);
 
-    $sql = "UPDATE rl_configs SET name = '".$name."', polarity=".$polarity.", enabled=".$enabled." WHERE id = ".$id."";
-    if ($conn->query($sql)!=true) {
-        echo "ERR: " . $sql . "<br>" . $conn->error;
-    } else {
-        snackbar("Done");
-    }
+    $conn = Connect();
+    $sql = "SELECT name FROM rl_modes WHERE id=".$mode."";
+    $result = $conn->query($sql);
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            $sel = $row["name"];
+  }
+}
+
+echo "<label>Modes</label>
+  <select name=\"mode\" class=\"w3-select\">\n";
+    $sql = "SELECT id,name FROM rl_modes";
+    $result = $conn->query($sql);
     $conn->close();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            if ($row["name"]==$sel) {
+            echo "<option value=\"".$row["id"]."\" selected>".$row["name"]."</option>\n";
+        }
+        else {
+            echo "<option value=\"".$row["id"]."\">".$row["name"]."</option>\n";
+        }
+        }
+    }
+    echo "</select>\n";
+}
+
+function do_operators()
+{
+    echo "<label>Function</label>
+          <select name=\"operator\" class=\"w3-select\">\n";
+    $conn    = Connect();
+    $sql = "SELECT id,name FROM rl_operators";
+    $result = $conn->query($sql);
+    $conn->close();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=\"".$row["id"]."\">".$row["name"]."</option>\n";
+        }
+    }
+    echo "</select>\n";
+}
+
+function do_sources()
+{
+    echo "<label>Sources</label>
+          <select name=\"source\" class=\"w3-select\">\n";
+    $conn    = Connect();
+    $sql = "SELECT id,name FROM rl_sources";
+    $result = $conn->query($sql);
+    $conn->close();
+    if ($result->num_rows > 0) {
+        while ($row = $result->fetch_assoc()) {
+            echo "<option value=\"".$row["id"]."\">".$row["name"]."</option>\n";
+        }
+    }
+    echo "</select>\n";
 }
 ?>
--- localstorage/modules/dashboard/dashboard_digital_update.php
+++ localstorage/modules/dashboard/dashboard_digital_update.php
@@ -1,6 +1,40 @@
 <?php
   require_once "../connection.php";
   require_once "../soc.php";
+  echo "<style>
+        .label {
+            display: inline-block;
+            padding: 0 20px;
+            height: 40px;
+            width: 100%;
+            font-size: 16px;
+            line-height: 40px;
+            border-radius: 20px;
+            background-color: #f1f1f1;
+        }
+
+        .reddot {
+            float: left;
+            margin: 5px 15px 0 -15px;
+            height: 30px;
+            width: 30px;
+            background-color: #f44336;
+            line-height: 30px;
+            border-radius: 50%;
+            display: inline-block;
+        }
+
+        .greendot {
+            float: left;
+            margin: 5px 15px 0 -15px;
+            height: 30px;
+            width: 30px;
+            background-color: #4CAF50;
+            line-height: 30px;
+            border-radius: 50%;
+            display: inline-block;
+        }
+    </style>";
   $conn = Connect();
   $sql = "SELECT di_configs.id, di_status.state, di_configs.name
   FROM di_status
@@ -10,14 +44,18 @@
   $conn->close();
   if ($result->num_rows > 0) {
       while ($row = $result->fetch_assoc()) {
+
           echo "<div class=\"w3-row-padding\">
+                    <div class=\"w3-col m12 s12\">\n";
+
+          /*
                     <div class=\"w3-col m2 s2\">\n";
-                    echo "<input class=\"w3-radio\" type=\"radio\"";
                     if ($row['state']) {
-                        echo " checked/>";
+                        echo "<span class=\"greendot\"></span>";
                     } else {
-                        echo "/>";
+                        echo "<span class=\"reddot\"></span>";
                     }
+
           echo "</div>";
           echo "<div class=\"w3-col m10 s10\">\n";
           echo "<div name=\"di".$row["id"]."\" class=\"w3-panel w3-round-large w3-border\"><p>".$row["name"];
@@ -27,9 +65,22 @@
               echo " OFF";
           }
           echo "</p></div>\n";
+          echo "</div>";
+          */
+          echo "<div class=\"label\">";
+          if ($row['state']) {
+              echo "<span class=\"greendot\"></span>";
+          } else {
+              echo "<span class=\"reddot\"></span>";
+          }
+            echo $row["name"];
+        echo "</div>";
+
+
+
           echo "</div>
-          </div>
-                \n";
+                </div>\n";
+                echo "<br/>";
       }
   }
 ?>
